6a73021ce6eada6dbd651753b7e7aa1a10a9c069
Miftah Mubarak <mubarakmiftah@gmail.com>
2022-12-25 11:22:11 +0700

    Define new theme

--- app/src/main/java/com/mimuslabs/ngompos/BasicLayoutActivity.kt
+++ app/src/main/java/com/mimuslabs/ngompos/BasicLayoutActivity.kt
@@ -8,13 +8,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Create
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mimuslabs.ngompos.R.string
 import com.mimuslabs.ngompos.ui.theme.NgomposTheme
 
 class BasicLayoutActivity : ComponentActivity() {
@@ -22,11 +32,22 @@ class BasicLayoutActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       NgomposTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Column {
-            BasicLayoutColumn()
-            BasicLayoutRow()
+        Scaffold(topBar = {
+          TopAppBar(navigationIcon = {
+            Icon(
+              imageVector = Icons.Rounded.Create, contentDescription = null,
+              modifier = Modifier.padding(horizontal = 12.dp)
+            )
+          }, title = {
+            Text(text = stringResource(id = string.app_name))
+          }, backgroundColor = MaterialTheme.colors.primarySurface)
+        }) {
+          // A surface container using the 'background' color from the theme
+          Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            Column {
+              BasicLayoutColumn()
+              BasicLayoutRow()
+            }
           }
         }
       }
@@ -41,26 +62,27 @@ fun BasicLayoutColumn() {
     verticalArrangement = Arrangement.Center,
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
-    Text(text = "Baris 1")
-    Text(text = "Baris 2")
-    Text(text = "Baris 3")
-    Text(text = "Baris 4")
-    Text(text = "Baris 5")
+    Text(text = "Baris 1", style = MaterialTheme.typography.h5)
+    Text(text = "Baris 2", style = MaterialTheme.typography.h4)
+    Text(text = "Baris 3", style = MaterialTheme.typography.h3)
+    Text(text = "Baris 4", style = MaterialTheme.typography.h2)
+    Text(text = "Baris 5", style = MaterialTheme.typography.h1)
   }
 }
 
 @Composable
 fun BasicLayoutRow() {
   Row {
-    Text(text = "Baris 1")
+    Text(text = "Baris 1", style = MaterialTheme.typography.body1)
     Text(text = "Baris 2")
     Text(text = "Baris 3")
     Text(text = "Baris 4")
     Text(text = "Baris 5")
+    Text(text = "Baris 6")
   }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewBasicLayout() {
   Column {
